58864e4719a82dae634b3613c60b41b59f2c3529
roshan8 <roshan.aloor@gmail.com>
2020-01-09 22:46:57 +0530

    Added CRUD operations for BoldDB bucket

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -31,4 +32,68 @@ func Init(dbPath string) error {
 		return err
 	})
 
+}
+
+func CreateTask(task string) (int, error) {
+	// Stores the task in BoltDB bucket
+
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	return 0, nil
+}
+
+func DeleteTask(key int) error {
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// AllTasks Returns every items in the bucket
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
